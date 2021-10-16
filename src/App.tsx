c76e5fb4059ae38aa1f4397e5454ca0a789fa341
yegyu <j.y.g.411@gmail.com>
2021-10-16 15:40:57 +0900

    url sink 다시 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
-import {  getYears, nowAsNextWeek, nowYear, setNotFoundMessage } from "./util/util";
+import { getYears, nowAsNextWeek, nowYear, setNotFoundMessage } from "./util/util";
 import { BrowserRouter, Link, Route, Router, useHistory } from "react-router-dom";
 import { HeaderView } from "./view/print/header";
 import SideBar from "./view/SideBar";
@@ -21,9 +21,9 @@ function App(linkState: AppProps) {
   const frontList = getYears();
   console.log(frontList);
 
-  useEffect(()=>{
+  useEffect(() => {
     updateUrlPath(linkState.link)
-  },[linkState.link])
+  }, [linkState.link])
   let view: JSX.Element;
   var notFountMessage: string | null = null;
 
@@ -32,43 +32,47 @@ function App(linkState: AppProps) {
   const [notFound, updateNotFound] = useState<string | null>(null);
   const [printViewContentsDto, setPrintViewContentDto] = useState<ContentsDto | null>(null);
   const [tempPrintViewContentsDto, setTempPrintViewContentDto] = useState<ContentsDto | null>(null);
-  
+
   const [tempYearMonth, updateTempYearMonth] = useState<string>("");
   const gitRoot = "yc_hanja_page"
   //init
-  if (window.location.pathname == "/" || window.location.pathname == `/${gitRoot}/`)
-  window.location.href = initMonthYear
-  
+  if (window.location.pathname == "/" ) {
+    window.location.href = initMonthYear
+  }
+
   var pathname = window.location.pathname;
- 
+  if(pathname.includes(`/${gitRoot}/`)){
+    pathname = initMonthYear
+  }
+
   console.log('pathname', pathname);
-  
+
   var [year, month, week]: string[] = pathname.split("/").filter(v => v != "");
   // if (!year) year = String(nowYear);
   console.log('filtered >>>> ', year, month, week);
   if (!week) {
     if (printViewContentsDto != null)
-    setPrintViewContentDto(null);
+      setPrintViewContentDto(null);
   }
-  
+
   const [monthFiles, updateMonthFiles] = useState<string[] | null>(null);
   useEffect(() => {
     getYearsJson(year).then((yaerJson: YearJson | null) => {
       console.log('getYearsJson>>>>>', yaerJson);
-      
+
       if (!yaerJson) return (<div>Not found</div>);
       updateMonthFiles(yaerJson.month_files);
     })
   }, [pathname]);
-  
+
   useEffect(() => {
-    if (!week){
+    if (!week) {
       console.log("not week");
-      
+
       setPrintViewContentDto(null);
-    } 
+    }
     else {
-      console.log("yes week",tempPrintViewContentsDto,"&");
+      console.log("yes week", tempPrintViewContentsDto, "&");
       setPrintViewContentDto(tempPrintViewContentsDto);
     }
   }, [week])
@@ -129,14 +133,14 @@ function App(linkState: AppProps) {
       view = (
         <div>
           <PrintView front_hanja_list={content.front_hanja_list}
-          main_words={content.main_words}
-          questions={content.questions}
-          yojeol={content.yojeol}
-          back_hanja_list={content.back_hanja_list}
-          week={content.week}
-          handleContentDto={content.handleContentDto}
-          month={parseInt(month)}
-          yaer={parseInt(year)} voca_list={content.voca_list}          />
+            main_words={content.main_words}
+            questions={content.questions}
+            yojeol={content.yojeol}
+            back_hanja_list={content.back_hanja_list}
+            week={content.week}
+            handleContentDto={content.handleContentDto}
+            month={parseInt(month)}
+            yaer={parseInt(year)} voca_list={content.voca_list} />
         </div>
       )
       return view;
